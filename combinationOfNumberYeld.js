aac46b2413b8aed142d456bed902853e4a3f9852
Ramachandra Joshi <ramueargoppa@gmail.com>
2020-05-29 20:36:27 +0530

    sanskrit grammer kind logic challenge added

--- combinationOfNumberYeld.js
+++ combinationOfNumberYeld.js
@@ -9,22 +9,20 @@ var inp = [2, 6, 10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58];
 
 var whichCombinationNumbers = (ip,combination,num) => {
   var p = Math.pow(2, ip.length);
-  var twoPower,
-    str = "";
+  var twoPower;
   for (i = 0; i < p; i++) {
     twoPower = p;
+    var values = [];
     for (j = 0; j < ip.length; j++) {
       twoPower = twoPower / 2;
-      str += i & twoPower ? ip[j] + "," : "";
+      if(i & twoPower){
+        values.push(ip[j])
+      }
     }
-
-    var values = str.split(",");
-    values.pop();
     if (values.length == combination) {
       var sum = values.reduce((prev, cur) => prev + Number.parseInt(cur), 0);
       if (sum == num) return values.join(",")+" numbers sum will yeld "+num;
     }
-    str = ""
   }
   return "no "+combination+" numbers sum will yeld "+num;
 };
